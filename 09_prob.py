8b8a292e4b2e9281fce5081f9ee3cddfb2bacbf6
“Arpita <arpitakumari10653@gmail.com>
2025-06-04 13:24:55 +0530

    make hollow ring with stars

---
+++ 09_prob.py
@@ -0,0 +1,18 @@
+'''
+
+* * *        for n = 3
+*   *
+* * *
+
+'''
+
+n = int(input("Enter a number: "))
+
+for i in range(1, n + 1):
+    if (i == 1 or i == n):
+        print("*" * n)
+    else:
+        print("*" + " " * (n - 2) + "*")
+
+
+  
